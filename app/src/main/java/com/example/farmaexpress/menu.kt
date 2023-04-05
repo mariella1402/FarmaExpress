96e27b9763a1074f066e303079f9b6e03976506f
mariella1402 <mariiellachiicas14@gmail.com>
2023-04-04 18:02:27 -0600

    registro y login de usuarios

--- app/src/main/java/com/example/farmaexpress/menu.kt
+++ app/src/main/java/com/example/farmaexpress/menu.kt
@@ -1,11 +1,27 @@
 package com.example.farmaexpress
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+
 
 class menu : AppCompatActivity() {
+    private lateinit var btCS: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+
+        btCS= findViewById(R.id.cerrarsesion)
+        btCS.setOnClickListener{
+            this.CerrarSesion()
+        }
+    }
+
+    private fun CerrarSesion(){
+        finish()
+        val intent = Intent(applicationContext, MainActivity::class.java)
+        startActivity(intent)
     }
 }
